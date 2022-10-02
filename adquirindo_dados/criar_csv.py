2f39f1a09510896bea5c06ef7df9df40b28d8a68
Luis Bueno <luisbuenopro@gmail.com>
2022-10-02 17:22:36 -0300

    Crie o csv e organize

--- adquirindo_dados/criar_csv.py
+++ adquirindo_dados/criar_csv.py
@@ -44,3 +44,49 @@ criar_arquivo_albuns_musicas()
 criar_arquivo_premiacoes()
 criar_arquivo_informacoes_musicas()
 criar_arquivo_vendas()
+
+"""
+Os arquivos foram unidos em somente um excel,
+com padronização de dados manual(como nome de musicas ou albuns diferentes)
+e utilização de funções do excel para facilitar esse processo
+
+Informações faltantes foram pesquisadas a mão na internet
+Informações adicionadas:
+
+relação album-musica
+
+album musica
+Barrett	Baby Lemonade
+Concept Album	Breakthrough
+Showing Feelings	Call The Schoolmaster
+Barrett	Golden Hair
+Hey Hey Rise Up	Hey Hey Rise Up
+The Wall	Late Night
+Barrett	Let's Roll Another One
+The Early Years	Lucy Leave
+The Piper at the Gates of Dawn	Matilda Mother (2010 Mix)
+London 666	Nick's Boogie
+Barrett	No Good Trying
+Barrett	Octupus
+Barrett	One In a Million
+Barrett	Opel
+Point Me At The Sky	Point Me At The Sky
+Wish You Were Here	Raving And Drooling
+The Early Years	Scream Thy Last Scream
+The Early Years	Seabirds
+Barrett	Swan Lee (Silas Lang)
+The Division Bell	The Division Bell
+The Final Cut	The Hero's Return (Pt. 2)
+The Return Of The Son Of Nothing	The Return Of The Son Of Nothing
+Barrett	Two Of a Kind
+Live at the Roundhouse	Vegetable Man
+Barrett	Wolfpack
+Wish You Were Here	You Gotta Be Crazy
+
+Albuns Acrescentados:
+
+A NICE PAIR	
+IS THERE ANYBODY OUT THERE? THE WALL: LIVE 1980-1981
+Shine On
+Discovery
+"""
