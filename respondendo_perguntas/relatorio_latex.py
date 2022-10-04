f5cad5a86440e382d0b2b2dbba0f5249a9253bb9
Anajuliarlc <anajuliaritalc@gmail.com>
2022-10-03 21:12:55 -0300

    termine de criar o relatotio do grupo 2 e organize o código

--- respondendo_perguntas/relatorio_latex.py
+++ respondendo_perguntas/relatorio_latex.py
@@ -3,15 +3,6 @@ from fpdf import FPDF
 import warnings
 import pandas as pd
 
-
-warnings.filterwarnings("ignore", category=DeprecationWarning)
-
-pdf = FPDF("P", "mm", "A4")
-pdf.add_page()
-pdf.set_auto_page_break(auto=True, margin = 15)
-pdf.set_font("helvetica", size=12)
-
-
 def add_texto(texto: str):
     """Função que pega o texto que é dado e imprime no pdf.
 
@@ -20,15 +11,8 @@ def add_texto(texto: str):
     :return: retorna a pergunta no pdf
     :rtype: PDF
     """    
-    pergunta = pdf.cell(0, 10, texto, ln=1)
-    return pergunta
-
-add_texto("Quais são as palavras mais comuns nos títulos dos Álbuns?")
-
-nome_arquivo = "informacoes_pink_floyd.xlsx"
-
-pergunta_1 = rg.top_3_pal_titulos_albuns(nome_arquivo)
-#print(pergunta_1)
+    texto_pdf = pdf.cell(0, 10, texto, ln=1)
+    return texto_pdf
 
 def add_cont_palavras(dataframe: pd.core.frame.DataFrame):
     """Recebe um dataframe e para cada indice dele, ele pega a linha. Para ser 
@@ -58,47 +42,52 @@ def add_cont_palavras_alb(dicionario: dict):
     for titulo, dataframe in dicionario.items():
         try:
             titulo = str(titulo)
+            add_texto("\n")
             add_texto(titulo)
             add_cont_palavras(dataframe)
         except:
             add_texto("Esse álbum não obedece o padrão UTF-8.")
 
-add_cont_palavras(pergunta_1)
-add_texto("\n\n")
-add_texto("Quais são as palavras mais comuns nos títulos das músicas?")
+def criar_relatorio():
+    warnings.filterwarnings("ignore", category=DeprecationWarning)
+    pdf = FPDF("P", "mm", "A4")
+    pdf.add_page()
+    pdf.set_auto_page_break(auto=True, margin = 15)
+    pdf.set_font("helvetica", size=12)
+    
+    nome_arquivo = "informacoes_pink_floyd.xlsx"
 
-pergunta_2 = rg.top_3_pal_titulos_musicas(nome_arquivo)
-#print(pergunta_2)
 
-add_cont_palavras(pergunta_2)
-add_texto("\n\n")
-add_texto("Quais são as palavras mais comuns nas letras das músicas, por Álbum?")
+    add_texto("Quais são as palavras mais comuns nos títulos dos Álbuns?")
+    pergunta_1 = rg.top_3_pal_titulos_albuns(nome_arquivo)
+    add_cont_palavras(pergunta_1)
+    add_texto("\n\n")
 
-pergunta_3 = rg.top_3_pal_albuns(nome_arquivo)
-#print(pergunta_3)
+    add_texto("Quais são as palavras mais comuns nos títulos das músicas?")
+    pergunta_2 = rg.top_3_pal_titulos_musicas(nome_arquivo)
+    add_cont_palavras(pergunta_2)
+    add_texto("\n\n")
+    
+    add_texto("Quais são as palavras mais comuns nas letras das músicas, por Álbum?")
+    pergunta_3 = rg.top_3_pal_albuns(nome_arquivo)
+    add_cont_palavras_alb(pergunta_3)
+    add_texto("\n\n")
 
-add_cont_palavras_alb(pergunta_3)
-add_texto("\n\n")
-add_texto("Quais são as palavras mais comuns nas letras das músicas, em toda a discografia?")
+    add_texto("Quais são as palavras mais comuns nas letras das músicas, em toda a discografia?")
+    pergunta_4 = rg.top_3_pal_todas_musicas(nome_arquivo)
+    add_cont_palavras(pergunta_4)
+    add_texto("\n\n")
 
-pergunta_4 = rg.top_3_pal_todas_musicas(nome_arquivo)
+    add_texto("O título de um álbum é tema recorrente nas letras?")
+    pergunta_5 = rg.tit_alb_recorrente_letras(nome_arquivo)
+    add_cont_palavras_alb(pergunta_5)
+    add_texto("\n\n")
 
-add_cont_palavras(pergunta_4)
-add_texto("\n\n")
-add_texto("O título de um álbum é tema recorrente nas letras?")
+    add_texto("O título de uma música é tema recorrente nas letras?")
+    pergunta_6 = rg.tit_mus_recorrente_letras(nome_arquivo)
+    add_cont_palavras_alb(pergunta_6)
+    add_texto("\n\n")
 
-pergunta_5 = rg.tit_alb_recorrente_letras(nome_arquivo)
-print(pergunta_5)
+    pdf.output("relatorio.pdf")
 
-#add_cont_palavras(pergunta_5) #modificar funcao
-add_texto("\n\n")
-add_texto("O título de uma música é tema recorrente nas letras?")
-
-pergunta_6 = rg
-#print(pergunta_6)
-
-#add_cont_palavras(pergunta_5) #modificar funcao
-add_texto("\n\n")
-
-pdf.output("relatorio.pdf")
 
